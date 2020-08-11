8558924d1cdfed90f700ef1ae19cd74b9d1844f1
Cuong <cuong@nete.com>
2020-08-11 18:31:47 -0400

    update

--- src/.vuepress/components/LeetcodePosts.vue
+++ src/.vuepress/components/LeetcodePosts.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="post-container">
         <div class="post-card" v-for="page in pages">
-            <router-link :to="page.path">
+            <!-- <router-link :to="page.path">
                 <div class="page-title">{{ page.title }}</div>
-            </router-link>
+            </router-link> -->
             <div class="page-detail">
                 <div v-html="page.excerpt"></div>
             </div>
@@ -19,7 +19,6 @@ export default {
     },
     mounted() {
         this.pages = this.$site.pages.filter(x => x.frontmatter.type === 'leetcode');
-        console.log(this.pages);
     }
 }
 </script>
