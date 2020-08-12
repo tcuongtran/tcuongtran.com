b26a20de530bce22335881a0bf7ff1938d94a3bc
Cuong Tran <tcuongtran@gmail.com>
2020-08-12 00:08:54 -0400

    moved to gatsbyjs

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://tcuongtran.com',
+  pathPrefix: '/',
+  title: 'Cuong Tran',
+  subtitle: '',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    // {
+    //   label: 'About me',
+    //   path: '/pages/about'
+    // },
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // }
+  ],
+  author: {
+    name: 'Cuong Tran',
+    photo: '/7048735.jpeg',
+    bio: 'Frontend Developer',
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: '',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
--- src/leetcode/Readme.md
+++
@@ -1,2 +0,0 @@
-# Leetcode submissions
-<LeetcodePosts />
